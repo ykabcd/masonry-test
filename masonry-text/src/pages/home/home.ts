2ff1af0a077a686b23ccd386c81a90fd00e7613c
kai <kabengka@gmail.com>
2018-08-06 23:37:34 +1000

    add imagesloaded

--- masonry-text/src/pages/home/home.ts
+++ masonry-text/src/pages/home/home.ts
@@ -7,7 +7,9 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   public masList=[];
-  
+  public Masonry: any;
+  public msnry: any;
+ 
   constructor(public navCtrl: NavController) {
     for(let i=0; i<10; i++){
 
@@ -15,15 +17,21 @@ export class HomePage {
 
     }
   }
-  Masonry: any; //outer scope
+  
   ionViewDidEnter() {
+    let instance = this;
     setTimeout(() => {
-    var elem = document.querySelector('.grid');
-    var msnry = new Masonry(elem, {
-    // options
-    itemSelector: '.grid-item',
-    fitWidth: true
-    });
+    
+    let elem = document.querySelector('.grid');
+    imagesLoaded(elem, function () {})
+      // all images loaded
+      //  instance.utils.debug("images loaded done, instantiating packery");
+    instance.msnry = new Masonry(elem, {
+         // options
+         itemSelector: '.grid-item',
+         percentPosition: true,  
+       });
+      
     }, 100);
-    }
+}
 }
